67e3714c19fa0367afa4eb9964a969a2ecc75488
diana96alazzam <diana-azzam@STDLTUC-L019.localdomain>
2020-04-09 17:17:19 +0300

    fixin and testin

--- modules/location.js
+++ modules/location.js
@@ -6,44 +6,42 @@ const superAgent = require('superagent');
 const helpers = require('./helpers.js');
 
 function locationFunction(request, response){
-    const city = request.query.city;
-    getLocation(city).then(data => helpers.renderStatus(data, response))
+    const searchedCity = request.query.city;
+    getLocation(searchedCity)
+    .then(data => helpers.renderStatus(data, response))
     .catch((error) => helpers.errorHandler(error, request, response));
 }
-function getLocation(city){
+function getLocation(searchedCity){
+    
     const SQL = 'SELECT * FROM locations WHERE search_query = $1';
-    const valueSQL = [city];
+    const valueSQL = [searchedCity];
     return client.query(SQL, valueSQL).then((searchResult) => {
 
         if (searchResult.rows.length > 0) {
             return(searchResult.rows[0]);
         } else {
             const geocodeAPIkey = process.env.GEOCODE_API_KEY; 
-            const locationAPIurl = (`https://eu1.locationiq.com/v1/search.php?key=${geocodeAPIkey}&q=${city}&format=json`);
+            const locationAPIurl = (`https://eu1.locationiq.com/v1/search.php?key=${geocodeAPIkey}&q=${searchedCity}&format=json`);
             
             return superAgent.get(locationAPIurl)
-
-                .then((locData) => {
-                    storeINdatabase(city, locData.body)
-                }).catch((error) =>helpers.errorHandler(error, request, response));
+                .then(locData => storeINdatabase(searchedCity, locData.body))
         }
-    })
+    });
 
 }
-function storeINdatabase(city, locData){
-    const locationData = new Location(city, locData)
-    const SQL = 'INSERT INTO locations (search_query, formatted_query, latitude, longitude) VALUES ($1,$2,$3,$4) RETURNING *';
-    const valueSQL = [locationData.search_query, locationData.formatted_query, locationData.latitude, locationData.longitude];
-    return client.query(SQL, valueSQL).then((results) => {
-        results.rows[0];
-    })
+function storeINdatabase(searchedCity, locData){
+    const locationData = new Location(locData[0]);
+    let SQL = 'INSERT INTO locations (search_query, formatted_query, latitude, longitude) VALUES ($1,$2,$3,$4) RETURNING *';
+    let valueSQL = [searchedCity, locationData.formatted_query, locationData.latitude, locationData.longitude];
+    return client.query(SQL, valueSQL)
+    .then(results => results.rows[0]);
 }
 
-function Location(city, locData) {
-    this.search_query = city;
-    this.formatted_query = locData[0].display_name;
-    this.latitude = locData[0].lat;
-    this.longitude = locData[0].lon;
+function Location(locData) {
+    this.search_query = locData.search_query;
+    this.formatted_query = locData.display_name;
+    this.latitude = locData.lat;
+    this.longitude = locData.lon;
 }
 
 
